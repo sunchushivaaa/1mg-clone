e2a473382ed2ebab939da257f1822fa1674e6e7c
Shiva Sunchu <chhotahathi06@gmail.com>
2023-02-21 13:02:13 +0530

    Initial commit

---
+++ erect-measure-8906/src/Redux/AdminSide/reducer.admin.js
@@ -0,0 +1,8 @@
+const initialState = {};
+export const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default: {
+      return state;
+    }
+  }
+};
--- erect-measure-8906/src/Redux/store.js
+++ erect-measure-8906/src/Redux/store.js
@@ -1,15 +1,14 @@
 import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
-import { reducer as UserCartReducer } from "./User side/Cart redux/reducer.cart";
-import { reducer as UserConsultReducer } from "./User side/Consult redux/reducer.consult";
-import { reducer as AminReducer } from "./Admin side/reducer.admin";
+import { reducer as UserCartReducer } from "./UserSide/Cart redux/reducer.cart";
+import { reducer as UserConsultReducer } from "./UserSide/Consult redux/reducer.consult";
+import { reducer as AdminReducer } from "./AdminSide/reducer.admin";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   UserCartReducer,
   UserConsultReducer,
-  AminReducer,
+  AdminReducer,
 });
 
-const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 
-export { store };
