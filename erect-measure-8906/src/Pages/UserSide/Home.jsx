de30c85123d4c87f7b8aaddba92806cc90343e6a
Shiva Sunchu <chhotahathi06@gmail.com>
2023-02-24 16:00:26 +0530

    Initial commit 2.0

--- erect-measure-8906/src/Pages/UserSide/Home.jsx
+++ erect-measure-8906/src/Pages/UserSide/Home.jsx
@@ -1,11 +1,10 @@
-import Slider from "react-slick"
+import Slider from "react-slick";
 import FeaturedBrands from "../../Components/UserSide/FeaturedBrands";
 import FullBody from "../../Components/UserSide/FullBody";
 import ShopHealth from "../../Components/UserSide/ShopHealth";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 export default function Home() {
-
   const Shop_by_health_concerns = [
     {
       img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/c2a0598f-483c-48ff-9783-71e402aa28d3.png",
@@ -42,10 +41,7 @@ export default function Home() {
     {
       img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/631e3dca-3d5b-49f8-832b-8341ec4784e7.png",
       name: "Eye Care",
-    }
-  
-
- 
+    },
   ];
   const slider1 = {
     dots: true,
@@ -54,32 +50,32 @@ export default function Home() {
     speed: 500,
     slidesToShow: 7,
     slidesToScroll: 4,
-    responsive:[
+    responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   const Featured_Brands = [
     {
@@ -118,9 +114,7 @@ export default function Home() {
       img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/ead930c9-e893-4777-a303-b6ec5e95c249.png",
       name: "TATA 1mg Health Products",
     },
-    
   ];
-
 
   const slider2 = {
     dots: true,
@@ -129,166 +123,153 @@ export default function Home() {
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 4,
-    responsive:[
+    responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   const Full_body_health_checkups = [
     {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Comprehensive Gold Full Body Checkup",
-    desc:"Includes 78 Tests",
-    rating:"4.9",
-    price:2099,
-    O_price:4198,
-    discount:"50 % off"
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Comprehensive Gold Full Body Checkup",
+      desc: "Includes 78 Tests",
+      rating: "4.9",
+      price: 2099,
+      O_price: 4198,
+      discount: "50 % off",
+    },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Good Health Smart Package",
+      desc: "Includes 3 Tests",
+      rating: "4.9",
+      price: 399,
+      O_price: 800,
+      discount: "50 % off",
+    },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Good Health Silver Package",
+      desc: "Includes 58 Tests",
+      rating: "4.9",
+      price: 649,
+      O_price: 1298,
+      discount: "50 % off",
+    },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Comprehensive Silver Full Body Checkup",
+      desc: "Includes 71 Tests",
+      rating: "4.9",
+      price: 1699,
+      O_price: 3398,
+      discount: "50 % off",
+    },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Women Wellness Premium Package",
+      desc: "Includes 46 Tests",
+      rating: "4.9",
+      price: 1799,
+      O_price: 3598,
+      discount: "50 % off",
+    },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Comprehensive Platinum Full Body Checkup",
+      desc: "Includes 91 Tests",
+      rating: "4.9",
+      price: 3499,
+      O_price: 6998,
+      discount: "50 % off",
+    },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Good Health Platinum Package",
+      desc: "Includes 65 Tests",
+      rating: "4.9",
+      price: 1299,
+      O_price: 2598,
+      discount: "50 % off",
+    },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Senior Citizen Advanced Package",
+      desc: "Includes 60 Tests",
+      rating: "4.9",
+      price: 1599,
+      O_price: 3198,
+      discount: "50 % off",
+    },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Men Health Premium Package",
+      desc: "Includes 44 Tests",
+      rating: "4.9",
+      price: 1799,
+      O_price: 3598,
+      discount: "50 % off",
+    },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Fever Package Extensive",
+      desc: "Includes 41 Tests",
+      rating: "4.9",
+      price: 999,
+      O_price: 1199,
+      discount: "16 % off",
+    },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Senior Citizen Comprehensive Package Male",
+      desc: "Includes 75 Tests",
+      rating: "4.9",
+      price: 3600,
+      O_price: 6900,
+      discount: "50 % off",
+    },
 
-  },
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Good Health Smart Package",
-    desc:"Includes 3 Tests",
-    rating:"4.9",
-    price:399,
-    O_price:800,
-    discount:"50 % off"
-
-  },
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Good Health Silver Package",
-    desc:"Includes 58 Tests",
-    rating:"4.9",
-    price:649,
-    O_price:1298,
-    discount:"50 % off"
-
-  },
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Comprehensive Silver Full Body Checkup",
-    desc:"Includes 71 Tests",
-    rating:"4.9",
-    price:1699,
-    O_price:3398,
-    discount:"50 % off"
-
-  },
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Women Wellness Premium Package",
-    desc:"Includes 46 Tests",
-    rating:"4.9",
-    price:1799,
-    O_price:3598,
-    discount:"50 % off"
-
-  },
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Comprehensive Platinum Full Body Checkup",
-    desc:"Includes 91 Tests",
-    rating:"4.9",
-    price:3499,
-    O_price:6998,
-    discount:"50 % off"
-
-  },
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Good Health Platinum Package",
-    desc:"Includes 65 Tests",
-    rating:"4.9",
-    price:1299,
-    O_price:2598,
-    discount:"50 % off"
-
-  },
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Senior Citizen Advanced Package",
-    desc:"Includes 60 Tests",
-    rating:"4.9",
-    price:1599,
-    O_price:3198,
-    discount:"50 % off"
-
-  },
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Men Health Premium Package",
-    desc:"Includes 44 Tests",
-    rating:"4.9",
-    price:1799,
-    O_price:3598,
-    discount:"50 % off"
-
-  },
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Fever Package Extensive",
-    desc:"Includes 41 Tests",
-    rating:"4.9",
-    price:999,
-    O_price:1199,
-    discount:"16 % off"
-
-  },
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Senior Citizen Comprehensive Package Male",
-    desc:"Includes 75 Tests",
-    rating:"4.9",
-    price:3600,
-    O_price:6900,
-    discount:"50 % off"
-
-  },
-  
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Senior Citizen Comprehensive Package Male",
-    desc:"Includes 75 Tests",
-    rating:"4.9",
-    price:3600,
-    O_price:6900,
-    discount:"47 % off"
-
-  },
-  {
-    img:"https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
-    name:"Good Health Gold Package",
-    desc:"Includes 63 Tests",
-    rating:"4.9",
-    price:949,
-    O_price:1898,
-    discount:"50 % off"
-
-  },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Senior Citizen Comprehensive Package Male",
+      desc: "Includes 75 Tests",
+      rating: "4.9",
+      price: 3600,
+      O_price: 6900,
+      discount: "47 % off",
+    },
+    {
+      img: "https://onemg.gumlet.io/diagnostics/b1f8f81e-5677-11ec-9fd9-0a65f29f7340.png?format=auto",
+      name: "Good Health Gold Package",
+      desc: "Includes 63 Tests",
+      rating: "4.9",
+      price: 949,
+      O_price: 1898,
+      discount: "50 % off",
+    },
   ];
 
   const slider3 = {
@@ -298,90 +279,70 @@ export default function Home() {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 4,
-    responsive:[
+    responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
-
-
-
-
-  
   return (
-    <div className="Home" style={{width:"100%",overflow:"hidden",padding:"20px"}}>
+    <div
+      className="Home"
+      style={{ width: "100%", overflow: "hidden", padding: "20px" }}
+    >
       <h1>Home Page</h1>
 
       <h3 className="tagname">Shop by health concerns</h3>
-     <div style={{background:"white "}}>
-       
-     <Slider {...slider1}>
-     
-     {Shop_by_health_concerns.map((item) => (
-       <ShopHealth
-         name={item.name}
-         img={item.img}
-        
-       />
-     ))}
-   </Slider>
-     </div>
-   <h3 className="tagname"> Featured Brands</h3>
+      <div style={{ background: "white " }}>
+        <Slider {...slider1}>
+          {Shop_by_health_concerns.map((item) => (
+            <ShopHealth name={item.name} img={item.img} />
+          ))}
+        </Slider>
+      </div>
+      <h3 className="tagname"> Featured Brands</h3>
       <Slider {...slider2}>
-     
-     {Featured_Brands.map((item) => (
-       < FeaturedBrands
-         name={item.name}
-         img={item.img}
-        
-       />
-     ))}
-   </Slider>
-   
-   <h3 className="tagname">Full body health checkups</h3>
+        {Featured_Brands.map((item) => (
+          <FeaturedBrands name={item.name} img={item.img} />
+        ))}
+      </Slider>
+
+      <h3 className="tagname">Full body health checkups</h3>
       <Slider {...slider3}>
-     
-     {Full_body_health_checkups.map((item) => (
-       < FullBody
-         name={item.name}
-         img={item.img}
-         desc={item.desc}
-         price={item.price}
-         O_price={item.O_price}
-         discount={item.discount}
-         rating={item.rating}
-
-        
-       />
-     ))}
-   </Slider>
-
-
-
-
+        {Full_body_health_checkups.map((item) => (
+          <FullBody
+            name={item.name}
+            img={item.img}
+            desc={item.desc}
+            price={item.price}
+            O_price={item.O_price}
+            discount={item.discount}
+            rating={item.rating}
+          />
+        ))}s
+      </Slider>
     </div>
   );
 }
