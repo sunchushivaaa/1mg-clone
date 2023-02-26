b75de40b3898fe150a91073bde5dc6bdbcbbf0d9
Dharmendra Yadav <kumardiren786@gmail.com>
2023-02-26 06:52:03 -0800

    change

--- erect-measure-8906/src/Components/UserSide/PrivateRoute.jsx
+++ erect-measure-8906/src/Components/UserSide/PrivateRoute.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-<<<<<<< Updated upstream
-import RedirectToLogin from '../../Pages/UserSide/RedirectToLogin';
-=======
+
 import { Navigate, useLocation } from 'react-router-dom';
 
->>>>>>> Stashed changes
 
 
 const PrivateRoute = ({children}) => {
     const{currentUser}=useSelector((store)=> store.AuthReducer)
-<<<<<<< Updated upstream
-
-    if(!currentUser){
-        return <RedirectToLogin />
-=======
     const location=useLocation();
      console.log(location)
     if(!currentUser){
     
         return <Navigate to="/login" state={location.pathname} replace/>
->>>>>>> Stashed changes
     }
   return children;
 }
--- erect-measure-8906/src/MainRoutes.jsx
+++ erect-measure-8906/src/MainRoutes.jsx
@@ -10,6 +10,9 @@ import DoctorComponent3 from "./Components/UserSide/DoctorComponent3";
 import Doctorsymptoms from "./Components/UserSide/Doctorsymptoms";
 import DoctorFinal from "./Components/UserSide/DoctorFinal";
 import Payment from "./Components/UserSide/Payment";
+import PrivateRoute from "./Components/UserSide/PrivateRoute";
+import Register from "./Pages/UserSide/Register";
+import FilterPage from "./Components/UserSide/FilterPage";
 
 export default function MainRoutes() {
   return (
@@ -18,26 +21,18 @@ export default function MainRoutes() {
       <Route path="/discover" element={<Discover />} />
       <Route path="/labtest" element={<LabTest />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/medicines" element={<Medicines />} />
+      <Route path="/medicines" element={<FilterPage />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/consultdoctor" element={<ConsultDoctor />} />
-<<<<<<< Updated upstream
-      <Route path="/abcd/" element={<DoctorComponent3/>}/>
-=======
-<<<<<<< Updated upstream
-      <Route path="/:type/:id" element={<ProductDetail />} />
-=======
+
+      
       <Route path="/abcd" element={<PrivateRoute><DoctorComponent3/></PrivateRoute>}/>
->>>>>>> Stashed changes
       <Route path="/symptoms" element={<Doctorsymptoms/>}/>
       <Route path="/doctor_final" element={<DoctorFinal/>}/>
       <Route path="/doctor_final" element={<DoctorFinal/>}/>
       <Route path="/payment" element={<Payment/>} />
-<<<<<<< Updated upstream
-=======
       <Route path="/register" element={<Register/>}/>
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+
       <Route path="*" element={<Error />} />
     </Routes>
   );
